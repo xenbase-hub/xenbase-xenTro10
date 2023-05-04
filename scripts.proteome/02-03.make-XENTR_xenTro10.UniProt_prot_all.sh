be20b8752bc64e6e47ca38c4732c881cc3cc3135
Taejoon Kwon <taejoon.kwon@gmail.com>
2023-05-04 13:38:02 -0400

    reorganize proteome script

--- scripts.proteome/02-03.make-XENTR_xenTro10.UniProt_prot_all.sh
+++ scripts.proteome/02-03.make-XENTR_xenTro10.UniProt_prot_all.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-DIR_RAW="UP.2023_02"
+DIR_RAW="raw"
 
 REL_VER=$(head -n 1 $DIR_RAW/relnotes.txt | awk '{print $3}')
 OUT_FILE="XENTR_xenTro10.UniProt_"$REL_VER".prot_all.fa"
 
-./02a.uniprot-cleanup-proteome.py $DIR_RAW/UP000008143_8364.fasta.gz > $OUT_FILE
-./02a.uniprot-cleanup-proteome.py $DIR_RAW/UP000008143_8364_additional.fasta.gz >> $OUT_FILE
-./03.make-non_redundant_fasta.py $OUT_FILE
+DIR_HERE=$(dirname $(realpath "$0"))
+
+$DIR_HERE/02a.uniprot-cleanup-proteome.py $DIR_RAW/UP000008143_8364.fasta.gz > $OUT_FILE
+$DIR_HERE/02a.uniprot-cleanup-proteome.py $DIR_RAW/UP000008143_8364_additional.fasta.gz >> $OUT_FILE
+$DIR_HERE/03.make-non_redundant_fasta.py $OUT_FILE
