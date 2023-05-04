86486210765ebbc7d0cb14ef1b36b59ec1cfa78d
Taejoon Kwon <taejoon.kwon@gmail.com>
2023-05-04 12:27:58 -0400

    move scripts for download to scripts.proteome

--- scripts.proteome/01.download-refseq.sh
+++ scripts.proteome/01.download-refseq.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-## UniProt
-UNIPROT_URL="https://ftp.uniprot.org/pub/databases/uniprot/current_release/"
-
-curl --output-dir ./raw -O "$UNIPROT_URL/relnotes.txt"
-curl --output-dir ./raw -O "$UNIPROT_URL/knowledgebase/reference_proteomes/Eukaryota/UP000008143/UP000008143_8364.fasta.gz"
-curl --output-dir ./raw -O "$UNIPROT_URL/knowledgebase/reference_proteomes/Eukaryota/UP000008143/UP000008143_8364_additional.fasta.gz"
-curl --output-dir ./raw -O "$UNIPROT_URL/knowledgebase/reference_proteomes/Eukaryota/UP000008143/UP000008143_8364.idmapping.gz"
-
 ## RefSeq
-
 curl --output-dir ./raw -O https://ftp.ncbi.nlm.nih.gov/genomes/refseq/vertebrate_other/Xenopus_tropicalis/representative/GCF_000004195.4_UCB_Xtro_10.0/GCF_000004195.4_UCB_Xtro_10.0_protein.faa.gz
 curl --output-dir ./raw -O https://ftp.ncbi.nlm.nih.gov/genomes/refseq/vertebrate_other/Xenopus_tropicalis/representative/GCF_000004195.4_UCB_Xtro_10.0/GCF_000004195.4_UCB_Xtro_10.0_rna.fna.gz
 curl --output-dir ./raw -O https://ftp.ncbi.nlm.nih.gov/genomes/refseq/vertebrate_other/Xenopus_tropicalis/representative/GCF_000004195.4_UCB_Xtro_10.0/GCF_000004195.4_UCB_Xtro_10.0_genomic.gff.gz
