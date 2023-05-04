4620baf71592ca10db465264ff2ee72503725a46
Taejoon Kwon <taejoon.kwon@gmail.com>
2023-05-04 12:11:05 -0400

    make uniprot scripts flexible to doownload other versions

--- 2023_04.proteome/01.download-refseq-uniprot.sh
+++ 2023_04.proteome/01.download-refseq-uniprot.sh
@@ -6,6 +6,7 @@ UNIPROT_URL="https://ftp.uniprot.org/pub/databases/uniprot/current_release/"
 curl --output-dir ./raw -O "$UNIPROT_URL/relnotes.txt"
 curl --output-dir ./raw -O "$UNIPROT_URL/knowledgebase/reference_proteomes/Eukaryota/UP000008143/UP000008143_8364.fasta.gz"
 curl --output-dir ./raw -O "$UNIPROT_URL/knowledgebase/reference_proteomes/Eukaryota/UP000008143/UP000008143_8364_additional.fasta.gz"
+curl --output-dir ./raw -O "$UNIPROT_URL/knowledgebase/reference_proteomes/Eukaryota/UP000008143/UP000008143_8364.idmapping.gz"
 
 ## RefSeq
 
--- 2023_04.proteome/02-03.make-XENTR_xenTro10.UniProt_2023_01_prot_all.sh
+++ 2023_04.proteome/02-03.make-XENTR_xenTro10.UniProt_2023_01_prot_all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-REL_VER=$(head -n 1 raw/relnotes.txt | awk '{print $3}')
+DIR_RAW="UP.2023_02"
+
+REL_VER=$(head -n 1 $DIR_RAW/relnotes.txt | awk '{print $3}')
 OUT_FILE="XENTR_xenTro10.UniProt_"$REL_VER".prot_all.fa"
-./02a.uniprot-cleanup-proteome.py raw/UP000008143_8364.fasta.gz > $OUT_FILE
-./02a.uniprot-cleanup-proteome.py raw/UP000008143_8364_additional.fasta.gz >> $OUT_FILE
+
+./02a.uniprot-cleanup-proteome.py $DIR_RAW/UP000008143_8364.fasta.gz > $OUT_FILE
+./02a.uniprot-cleanup-proteome.py $DIR_RAW/UP000008143_8364_additional.fasta.gz >> $OUT_FILE
 ./03.make-non_redundant_fasta.py $OUT_FILE
