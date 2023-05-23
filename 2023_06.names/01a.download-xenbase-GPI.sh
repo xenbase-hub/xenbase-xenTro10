03f91f2b7e0579f951a1fe1b2d3d199929bf6c82
Taejoon Kwon <taejoon.kwon@gmail.com>
2023-05-23 15:59:07 -0400

    a script to download Xenbase GPI records

---
+++ 2023_06.names/01a.download-xenbase-GPI.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DATE=$(date +"%Y-%m-%d")
+
+GPI_OUT="Xenbase_GPI."$DATE".raw.tsv"
+
+if [ ! -e $GPI_OUT ]; then
+  echo "Download $GPI_OUT"
+  curl --output-dir ./raw -o $GPI_OUT https://xenbase-bio1.ucalgary.ca/cgi-bin/reports/gpi_v2.cgi
+else
+  echo "$GPI_OUT is available. Skip."
+fi
