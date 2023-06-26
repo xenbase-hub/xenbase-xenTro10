77811faa2476ac2cc3b4f42153bdef3f5ea30ef3
Taejoon Kwon <taejoon.kwon@gmail.com>
2023-06-26 15:36:54 -0400

    compress the Xenbase GPI file after downloading

--- xenbase/01.download-xenbase-GPI.sh
+++ xenbase/01.download-xenbase-GPI.sh
@@ -7,6 +7,7 @@ GPI_OUT="Xenbase_GPI."$DATE".raw.tsv"
 if [ ! -e $GPI_OUT ]; then
   echo "Download $GPI_OUT"
   curl --output-dir ./raw -o $GPI_OUT https://xenbase-bio1.ucalgary.ca/cgi-bin/reports/gpi_v2.cgi
+  gzip ./raw/$GPI_OUT
 else
   echo "$GPI_OUT is available. Skip."
 fi
