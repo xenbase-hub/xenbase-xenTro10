0290333642452a5fa6ab86579d18ead7f59d6adb
Taejoon Kwon <taejoon.kwon@gmail.com>
2023-06-26 04:20:47 -0400

    use FILES.UP to download files

--- uniprot/01.download-uniprot.sh
+++ uniprot/01.download-uniprot.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-## UniProt
-UNIPROT_URL="https://ftp.uniprot.org/pub/databases/uniprot/current_release/"
+DATE=$(date +"%Y-%m-%d")
+echo "Today: "$DATE
+
+## UniProt current_release
+UNIPROT_URL="https://ftp.uniprot.org/pub/databases/uniprot/current_release"
 
 curl --output-dir ./raw -O "$UNIPROT_URL/relnotes.txt"
-curl --output-dir ./raw -O "$UNIPROT_URL/knowledgebase/reference_proteomes/Eukaryota/UP000008143/UP000008143_8364.fasta.gz"
-curl --output-dir ./raw -O "$UNIPROT_URL/knowledgebase/reference_proteomes/Eukaryota/UP000008143/UP000008143_8364_additional.fasta.gz"
-curl --output-dir ./raw -O "$UNIPROT_URL/knowledgebase/reference_proteomes/Eukaryota/UP000008143/UP000008143_8364.idmapping.gz"
-curl --output-dir ./raw -O "$UNIPROT_URL/knowledgebase/reference_proteomes/Eukaryota/UP000008143/UP000008143_8364.gene2acc.gz"
+RELNOTE_OUT="./raw/uniprot_relnotes.$DATE.txt"
+mv ./raw/relnotes.txt $RELNOTE_OUT
+
+UP_VERSION=$(head -n 1 $RELNOTE_OUT | awk '{print $3}')
+
+UNIPROT_XT_URL=$UNIPROT_URL"/knowledgebase/reference_proteomes/Eukaryota/UP000008143/"
+
+for FILENAME in $(cat FILES.UP000008143)
+do
+  FILENAME_OUT=$UP_VERSION"."$FILENAME
+  if [ -e "./raw/$FILENAME_OUT" ]; then
+    echo "$FILENAME_OUT exists. Skip."
+  else
+    echo "Download $FILENAME_OUT"
+    curl --output-dir ./raw -o $FILENAME_OUT "$UNIPROT_XT_URL/$FILENAME"
+  fi
+done
